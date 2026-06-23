0f53f2e1c7f672398241dff23bfc6352dc195578
Sirokoto <santovov56@mail.ru>
2026-06-24 01:39:39 +0700

    Added ^ operator that powers to exponent, comparing manually calculation vs my library.

--- complex.hpp
+++ complex.hpp
@@ -1,22 +1,39 @@
+#include <cmath>
+#include <cstdlib>
 #include <ostream>
+#include <stdexcept>
 #ifndef _COMPLEX_H_
 #define _COMPLEX_H_
-template<typename T> class Complex {
+
+template <typename T> class Complex {
 private:
   T a;
   T b;
+
 public:
   Complex(T a_re, T b_im) : a(a_re), b(b_im) {};
-
+  Complex(T par) : a(par), b(par) {};
   ~Complex() {};
-  
-  Complex<T> Conjugated() const {
-    return Complex<T>{a, -b};
-  }
 
+  T mod = this->Module();
+  T ang = this->GetAngle();
+
+  Complex<T> Conjugated() const { return Complex<T>{a, -b}; }
+  T Module() const { return (T)(std::abs(std::sqrt((a * a) + (b * b)))); }
+  T GetRe() const { return a; }
+  T GetIm() const { return b; }
+  T GetAngle() const { return (T)std::atan(this->b / this->a); };
   friend constexpr Complex<T> operator+(const Complex<T> &lhs,
                                         const Complex<T> &rhs) {
     return Complex<T>{lhs.a + rhs.a, lhs.b + rhs.b};
+  }
+  friend constexpr Complex<T> operator^(Complex<T> &lhs, const int &rhs) {
+    if (lhs.a == 0) {
+	  // TODO: Not implemented yet!
+	  throw std::logic_error("Operation is not implemented.");
+	}
+    return Complex<T>{(lhs.mod * lhs.mod) * std::cos(lhs.ang * rhs),
+                      (lhs.mod * lhs.mod) * std::sin(lhs.ang * rhs)};
   }
 
   friend constexpr Complex<T> operator-(const Complex<T> &lhs,
@@ -26,24 +43,24 @@ public:
 
   friend constexpr Complex<T> operator*(const Complex<T> &lhs,
                                         const Complex<T> &rhs) {
-    return Complex<T>{
-      (lhs.a * rhs.a) - (lhs.b * rhs.b),
-       (lhs.a * rhs.b) + (lhs.b * rhs.a)
-          };
+    return Complex<T>{(lhs.a * rhs.a) - (lhs.b * rhs.b),
+                      (lhs.a * rhs.b) + (lhs.b * rhs.a)};
+  }
+  friend constexpr Complex<T> operator*(const Complex<T> &lhs, const T &rhs) {
+    return Complex<T>{lhs.a * rhs, lhs.b * rhs};
   }
 
   friend constexpr Complex<T> operator/(const Complex<T> &lhs,
                                         const Complex<T> &rhs) {
-	Complex<T> intermediate = rhs*rhs.Conjugated();
-    T conj = (intermediate.a*intermediate.a+intermediate.b*intermediate.b);
-    return Complex<T> {
-      (
-	   ((lhs.a * rhs.a) + (lhs.b * rhs.b)) / conj),
-	  (((lhs.b * rhs.a) - (lhs.a * rhs.b)) / conj)
-	};
+    Complex<T> intermediate = rhs * rhs.Conjugated();
+    T conj =
+        (intermediate.a * intermediate.a + intermediate.b * intermediate.b);
+    return Complex<T>{(((lhs.a * rhs.a) + (lhs.b * rhs.b)) / conj),
+                      (((lhs.b * rhs.a) - (lhs.a * rhs.b)) / conj)};
   }
 
-  friend constexpr std::ostream &operator<<(std::ostream &a, const Complex<T> &c) {
+  friend constexpr std::ostream &operator<<(std::ostream &a,
+                                            const Complex<T> &c) {
     if (c.b > 0 && c.a != 0)
       a << c.a << " + " << c.b << "i";
     else if (c.b < 0 && c.a != 0)
@@ -52,6 +69,8 @@ public:
       a << c.b << "i";
     else if (c.b == 0)
       a << c.a;
+    else if (c.a == 0 && c.b == 0)
+	  a << 0;
     return a;
   };
 };
--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
-#include <iostream>
 #include "complex.hpp"
+#include <iostream>
+
+void ManuallCalculation(double real, double imag) {
+  int n = 2; // Exponent
+  double r = sqrt((real * real) + (imag * imag)); // Module or Distance or Hypotenuse
+  double ang = std::atan(imag / real); // Angle between Complex number as Vector and Ox;
+  double RealTrigPart = (r * r) * cos(ang * n); // Trigonometric Real Part of Complex number
+  double ImagTrigPart = (r * r) * sin(ang * n); // Trigonometric Imaginary Part of Complex number
+  // Check for output
+  if (ImagTrigPart > 0 && RealTrigPart != 0)
+    std::cout << RealTrigPart << " + " << ImagTrigPart << "i";
+  else if (ImagTrigPart < 0 && RealTrigPart != 0)
+    std::cout << RealTrigPart << " - " << -ImagTrigPart << "i";
+  if (RealTrigPart == 0)
+    std::cout << ImagTrigPart << "i";
+  else if (ImagTrigPart == 0)
+    std::cout << RealTrigPart;
+  std::cout << std::endl;
+}
 
 int main() {
-  Complex<int> a(-4,1);
-  Complex<int> b(4, 5);  
-  std::cout << a/b << std::endl;
-  std::cout << a << "\n" << a.Conjugated() << std::endl;
+  Complex<double> a(-4, 1);
+  Complex<double> b(0);
+  double RealPart = -4;
+  double ImagPart = 1;
+  std::cout << "Manually calculated:\n";
+  ManuallCalculation(RealPart, ImagPart);
+  std::cout << "My library:\n";
+  std::cout << (a ^ 2) << std::endl;
+  std::cout << b << std::endl;
   return 0;
 }
